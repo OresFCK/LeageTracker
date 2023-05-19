a5a5d1b0ab86264bd75321f34c91fa5611274dc9
oresFCK <kappa123.wow@gmail.com>
2023-05-19 10:29:47 +0200

    react router testing and needed libraries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import ExampleComponent from './test';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Blogs from "./pages/blogs"
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <div className="App">
       <h1>Example Component</h1>
-      <ExampleComponent />
     </div>
+    <div style={{marginLeft:'25px'}}>
+    <ExampleComponent />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </div>
+    </div>
+    
   );
 }
 
