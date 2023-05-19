d8a11d33e50e13e5f3e1e1df1412ea594d8dd54a
oresFCK <kappa123.wow@gmail.com>
2023-05-19 14:51:40 +0200

    import fix

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,6 @@
 import  React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -12,18 +14,14 @@ const FormContainer = styled.div`
   text-align: center;
 `;
 
-const UserInput = styled.TextField`
-  display: block;
-  margin-bottom: 15px;
-`;
 
 const Login = () => {
     return <Container>
     <FormContainer>
         <h1>Login</h1>
-            <UserInput id="outlined-basic" label="Email" variant="outlined" />
-            <UserInput id="outlined-basic" label="Password" variant="outlined" />
-            <UserInput variant="contained">Login</UserInput>  
+            <TextField sx={{display:'block', marginBottom:'15px'}} id="outlined-basic" label="Email" variant="outlined" />
+            <TextField sx={{display:'block', marginBottom:'15px'}} id="outlined-basic" label="Password" variant="outlined" />
+            <Button sx={{display:'block', marginBottom:'15px'}} variant="contained">Login</Button>  
         </FormContainer>
     </Container>
   };
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,4 +1,6 @@
 import  React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -12,19 +14,14 @@ const FormContainer = styled.div`
   text-align: center;
 `;
 
-const UserInput = styled.TextField`
-  display: block;
-  margin-bottom: 15px;
-`;
-
 const Register = () => {
     return <Container>
     <FormContainer>
         <h1>Registration</h1>
-            <UserInput id="outlined-basic" label="Nickname" variant="outlined" />
-            <UserInput id="outlined-basic" label="Email" variant="outlined" />
-            <UserInput id="outlined-basic" label="Password" variant="outlined" />
-            <UserInput variant="contained">Register</UserInput>  
+            <TextField sx={{display:'block', marginBottom:'15px'}} id="outlined-basic" label="Nickname" variant="outlined" />
+            <TextField sx={{display:'block', marginBottom:'15px'}} id="outlined-basic" label="Email" variant="outlined" />
+            <TextField sx={{display:'block', marginBottom:'15px'}} id="outlined-basic" label="Password" variant="outlined" />
+            <Button sx={{display:'block', marginBottom:'15px'}} variant="contained">Register</Button>  
         </FormContainer>
     </Container>
     
